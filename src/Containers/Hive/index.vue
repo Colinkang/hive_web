6ec9112d99af786a3e5837fdbd2abd3ddcf5c944
huangzhimao <zhimao.huang@evenhidata.com>
2018-06-10 18:15:07 +0800

    .

--- src/Containers/Hive/index.vue
+++ src/Containers/Hive/index.vue
@@ -107,7 +107,7 @@
     </div>
   </div>
   <!-- <div class="hive-left">
-  
+
     <div class="block">
       <span class="demonstration">选择时间</span>
       <el-date-picker v-model="date" type="datetimerange" range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期">
@@ -118,7 +118,7 @@
 
       <fold refs="fold"></fold>
     </div>
-   
+
       <div class="hive-pie">
         <echartspie ref="hive"></echartspie>
       </div>
@@ -261,7 +261,7 @@ export default {
 					};
 					// 画扇形图
 					_this.$refs.hive.drawLine(obj);
-					
+
 					// 将值赋值给列表
 					for (let obj of data) {
 						if (obj.status === 0) obj.status = '正在运行';
